b6eff5c02f3f4afa13fafdce7729d4c5c9d78e71
Abdyrahmanov Timurlan <timurlanabdyrahmanov7@gmail.com>
2021-08-18 13:21:58 +0600

    link

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,19 +3,19 @@ const Navbar = ()=>{
     return (
         <ul className="nav">
             <li>
-                <a href="#" className='menu'>Доставка</a>
+                <a href="https://www.google.com/" className='menu'>Доставка</a>
             </li>
             <li>
-                <a href="#" className='menu'>Оплата</a>
+                <a href="https://www.google.com/" className='menu'>Оплата</a>
             </li>
             <li>
-                <a href="#" className='menu'>Отзывы</a>
+                <a href="https://www.google.com/" className='menu'>Отзывы</a>
             </li>
             <li>
-                <a href="#" className='menu'>Вопрос - ответ</a>
+                <a href="https://www.google.com/" className='menu'>Вопрос - ответ</a>
             </li>
             <li>
-                <a href="#" className='menu'>Контакты</a>
+                <a href="https://www.google.com/" className='menu'>Контакты</a>
             </li>
         </ul>
     )
